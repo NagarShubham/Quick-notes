c45c244808b046c7cb34ed810caa7baad10e9ecd
Shubham <creatoshubham@gmail.com>
2025-04-05 00:37:35 +0530

    Added real card text and optimize code

--- app/src/main/java/com/example/quicknotes/ui/HomeScreen.kt
+++ app/src/main/java/com/example/quicknotes/ui/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.example.quicknotes.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,103 +8,101 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import com.example.quicknotes.R
 import com.example.quicknotes.common.NavigationRoutes
+import com.example.quicknotes.db.NotesModel
 import com.example.quicknotes.ui.theme.Typography
 import com.example.quicknotes.ui.viewmodel.HomeViewModel
+
+private val distance = 10.dp
 
 //@Preview(showBackground = true)
 @Composable
 fun HomeScreen(navController: NavHostController, homeViewModel: HomeViewModel) {
     var notesList = homeViewModel.getAllNotesList().collectAsStateWithLifecycle(emptyList()).value
 
-
-    val distance = 12.dp
-    val isGrid = remember {
-        mutableStateOf(true)
-    }
+    var isGrid by remember { mutableStateOf(true) }
 
     Column(
         Modifier
             .fillMaxSize()
-            .padding(start = distance, end = distance, top = 16.dp)
+            .padding(horizontal = 4.dp)
     ) {
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .height(40.dp)
-                .clip(CircleShape)
-                .background(Color.LightGray)
-                .padding(vertical = 4.dp, horizontal = 10.dp)
-                .wrapContentHeight(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-
-        ) {
-            Text(
-                "Search", modifier = Modifier
-                    .padding(start = 5.dp),
-                style = Typography.labelSmall
-            )
-
-            Icon(
-                painter = painterResource(if (isGrid.value) R.drawable.ic_list_action else R.drawable.ic_round_grid_view),
-                contentDescription = "Grid",
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .size(24.dp)
-                    .clickable {
-                        navController.navigate(NavigationRoutes.AddNote.route)
-                        //isGrid.value = isGrid.value.not()
-                    }
-            )
+        HomeHeader(isGrid = isGrid, onGridClick = { isGrid = !isGrid }) {
+            navController.navigate(NavigationRoutes.AddNote.route)
         }
-
         LazyVerticalGrid(
-            columns = GridCells.Fixed(if (isGrid.value) 2 else 1), // Set the number of rows
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = distance),
+            columns = GridCells.Fixed(if (isGrid) 2 else 1), // Set the number of rows
+            modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(distance),
             horizontalArrangement = Arrangement.spacedBy(distance)
-        ) {
-            items(notesList.size) { index ->
-                NotesCard()
-            }
-        }
-
+        ) { items(notesList) { NotesCard(it) } }
     }
 }
 
+@Preview(showBackground = false)
+@Composable
+private fun HomeHeader(
+    isGrid: Boolean = true,
+    onGridClick: () -> Unit = {},
+    onSearchClick: () -> Unit = {}
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = 14.dp)
+            .border(1.dp, Color.LightGray, RoundedCornerShape(8.dp))
+            .padding(horizontal = 10.dp, vertical = 10.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "Search",
+            modifier = Modifier
+                .weight(1f)
+                .clickable(onClick = onSearchClick),
+            style = Typography.labelLarge
+        )
+
+        Icon(
+            painter = painterResource(id = if (isGrid) R.drawable.ic_list_action else R.drawable.ic_round_grid_view),
+            contentDescription = "Grid",
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(24.dp)
+                .padding(2.dp)
+                .clickable(onClick = onGridClick)
+        )
+    }
+}
 
 //@Preview(showBackground = true)
 @Composable
-private fun NotesCard() {
-    val str =
-        "High level element that displays text and provides semantics / accessibility information.\n" +
-                "The default style uses the LocalTextStyle provided by the MaterialTheme / components. If you are setting your own style, you may want to consider first retrieving LocalTextStyle, and using TextStyle. copy to keep any theme defined attributes, only modifying the specific attributes you"
+private fun NotesCard(model: NotesModel) {
     Column(
         Modifier
             .defaultMinSize(minWidth = 120.dp, minHeight = 120.dp)
@@ -113,13 +110,13 @@ private fun NotesCard() {
             .padding(14.dp)
     ) {
         Text(
-            str,
+            model.title,
             modifier = Modifier.fillMaxWidth(),
             style = Typography.labelLarge,
             maxLines = 2, overflow = TextOverflow.Ellipsis
         )
         Text(
-            str,
+            model.note,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 10.dp),
