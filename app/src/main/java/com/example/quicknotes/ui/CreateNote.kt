b87b0dfdab6859c225ae4af8e922f9bcd8376e4c
Shubham <creatoshubham@gmail.com>
2025-04-06 00:04:03 +0530

    Optimized code.

--- app/src/main/java/com/example/quicknotes/ui/CreateNote.kt
+++ app/src/main/java/com/example/quicknotes/ui/CreateNote.kt
@@ -21,10 +21,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.quicknotes.R
 import com.example.quicknotes.db.NotesModel
+import com.example.quicknotes.ui.theme.Typography
 import com.example.quicknotes.ui.viewmodel.HomeViewModel
 
 //@Preview(showBackground = true)
@@ -33,67 +35,68 @@ fun CreateNote(navController: NavHostController, homeViewModel: HomeViewModel) {
     var titleText by remember { mutableStateOf("") }
     var noteText by remember { mutableStateOf("") }
 
-
-
     Column(Modifier.fillMaxSize()) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-
-            Icon(
-                painter = painterResource(R.drawable.ic_arrow_back),
-                contentDescription = "BackToHome",
-                modifier = Modifier.clickable { navController.popBackStack() }
-            )
-
-            Icon(
-                painter = painterResource(R.drawable.ic_arrow_back),
-                contentDescription = "Delete"
-            )
-
-            Text("Save", modifier = Modifier.clickable {
-                if (titleText.isEmpty() && noteText.isEmpty()) return@clickable
-
-                homeViewModel.addNote(
-                    NotesModel(
-                        title = titleText,
-                        note = noteText,
-                        updatedAt = System.currentTimeMillis()
-                    )
+        CreateHeader(onBackPress = { navController.popBackStack() }, onSavePress = {
+            if (titleText.isEmpty() && noteText.isEmpty()) return@CreateHeader
+            homeViewModel.addNote(
+                NotesModel(
+                    title = titleText,
+                    note = noteText,
+                    updatedAt = System.currentTimeMillis()
                 )
-                navController.popBackStack()
-            })
-        }
+            )
+            navController.popBackStack()
+        })
 
         HorizontalDivider(
             thickness = 1.dp,
             color = Color.Gray
         )
-
         TextField(
             value = titleText,
-            onValueChange = { newText -> titleText = newText },
-            placeholder = { Text("Title") },
+            onValueChange = { titleText = it },
+            placeholder = { Text(stringResource(R.string.title)) },
+            modifier = Modifier.fillMaxWidth(),
+            textStyle = Typography.labelLarge,
             colors = TextFieldDefaults.colors(
                 unfocusedContainerColor = Color.Transparent,
                 unfocusedIndicatorColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
             )
         )
-
         TextField(
             value = noteText,
-            onValueChange = { newText -> noteText = newText },
-            placeholder = { Text("Note") },
+            onValueChange = { noteText = it },
+            placeholder = { Text(stringResource(R.string.note)) },
+            modifier = Modifier.fillMaxWidth(),
+            textStyle = Typography.labelLarge,
             colors = TextFieldDefaults.colors(
                 unfocusedContainerColor = Color.Transparent,
                 unfocusedIndicatorColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
             )
         )
     }
+}
 
+@Composable
+private fun CreateHeader(onBackPress: () -> Unit, onSavePress: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
 
+        Icon(
+            painter = painterResource(R.drawable.ic_arrow_back),
+            contentDescription = "BackToHome",
+            modifier = Modifier.clickable(onClick = onBackPress)
+        )
+
+        Text(stringResource(R.string.save), modifier = Modifier.clickable(onClick = onSavePress))
+    }
 }
